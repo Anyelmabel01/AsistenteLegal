79f8d42c763c8d4ca927057ccb51e7f7a763dd7e
AnyeImabel01 <anyelmabell@gmail.com>
2025-04-17 10:10:37 -0500

    Initial project structure setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Asistente Legal Panamá</h1>
+      <p>Configuración inicial del proyecto.</p>
+    </div>
+  );
+}
+
+export default App; 
